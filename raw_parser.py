733493b7aae1182b71c27180011084ace1a87a89
AlvinAhmadov <erwin.dev.howard@gmail.com>
2019-01-18 15:04:04 +0400

    Local

--- raw_parser.py
+++ raw_parser.py
@@ -3,15 +3,16 @@ from collections import defaultdict
 
 class SDParser:
     def __init__(self, *args, **kwargs):
-        self.__filename = kwargs.pop('filename')
-        self.__separator = kwargs.pop('sep')
-        self.__dictionary = list()
+        self._filename = kwargs.pop('filename')
+        self._separator = kwargs.pop('sep')
+        self._dictionary = list()
         self.line_count = 0
-        self._count_lines()
-        self._keyword = ""
+        self.count_lines()
+
+    keyword = ""
 
     def __delete__(self, instance):
-        self.__dictionary.clear()
+        self._dictionary.clear()
         del instance
 
     def extract_words(self, key_index = 0, word_index = 2, start = 0):
@@ -25,29 +26,29 @@ class SDParser:
         return morph_map
 
     def parse_lines(self, start = 0, word_index = 2, paradigm_index = 3, key_index = 0):
-        self.__dictionary.insert(0, self.extract_words(key_index, word_index, start))
-        self.__dictionary.insert(1, self.extract_paradigms(key_index, paradigm_index, start))
-        return self.__dictionary
+        self._dictionary.insert(0, self.extract_words(key_index, word_index, start))
+        self._dictionary.insert(1, self.extract_paradigms(key_index, paradigm_index, start))
+        return self._dictionary
 
     def parse_line(self, line_index):
         index = 0
-        with open(self.__filename, 'r') as file:
+        with open(self._filename, 'r') as file:
             for line in file:
                 if line_index == index:
                     return line
                 index += 1
 
-    def _count_lines(self):
-        with open(self.__filename, 'r') as f:
+    def count_lines(self):
+        with open(self._filename, 'r') as f:
             for l in f:
                 self.line_count += 1
             f.close()
 
     def _extract(self, data_map, key_index, value_index, start = 0):
         for start in range(self.line_count - start):
-            data_list = self.parse_line(start).split(self.__separator)
-            if self._keyword not in data_list:
-                self._keyword = data_list[key_index]
-                data_map[self._keyword].append(data_list[value_index])
+            data_list = self.parse_line(start).split(self._separator)
+            if self.keyword not in data_list:
+                self.keyword = data_list[key_index]
+                data_map[self.keyword].append(data_list[value_index])
             else:
-                data_map[self._keyword].append(data_list[value_index])
+                data_map[self.keyword].append(data_list[value_index])
