30c0fc582c29ebdab948def7bd95e589793a69be
AlvinAhmadov <erwin.dev.howard@gmail.com>
2019-01-18 15:01:42 +0400

    Local

--- database_wrapper.py
+++ database_wrapper.py
@@ -1,19 +1,18 @@
 from sqlalchemy import create_engine, Table, MetaData, Column, DefaultClause
-from sqlalchemy.engine.base import Connection
 
 
 class DatabaseWrapper:
     def __init__(self, connection_name: str):
         self._engine = create_engine(connection_name, echo = False)
-        self.__connector = self._engine.connect()
-        self.__metadata = MetaData()
-        self.__tables = dict()
+        self._connector = self._engine.connect()
+        self._metadata = MetaData()
+        self._tables = dict()
 
     def __delete__(self, instance):
-        self.__connector.close()
-        del self.__connector
+        self._connector.close()
+        del self._connector
         del self._engine
-        del self.__metadata
+        del self._metadata
 
     def create_table(self, table_name, columns, drop = True):
         """ Create tables for database
@@ -21,18 +20,18 @@ class DatabaseWrapper:
          :param columns list of column objects
          :param drop Drop existing values
          """
-        self.__tables[table_name] = Table(table_name, self.__metadata, *columns)
+        self._tables[table_name] = Table(table_name, self._metadata, *columns)
         if drop:
-            self.__metadata.drop_all(self._engine)
+            self._metadata.drop_all(self._engine)
         else:
-            self.__metadata.create_all(self._engine)
+            self._metadata.create_all(self._engine)
         self.update_table(table_name)
         return table_name
 
     def update_table(self, table_name):
-        print(self.__metadata.get_children())
+        print(self._metadata.get_children())
         Table.select()
-        for col in self.__tables[table_name].primary_key:
+        for col in self._tables[table_name].primary_key:
             print(col.key)
         pass
 
@@ -40,15 +39,15 @@ class DatabaseWrapper:
         return self._engine.has_table(table_name)
 
     def drop_table(self, table_name):
-        self.__metadata.remove(self.__tables[table_name])
+        self._metadata.remove(self._tables[table_name])
         pass
 
     def insert_values(self, table_name, values: dict):
-        ins = self.__tables[table_name].insert().values(values)
-        self.__connector.execute(ins)
+        ins = self._tables[table_name].insert().values(values)
+        self._connector.execute(ins)
 
     def select_table(self, table_name):
-        self.__connector.execute(self.__tables[table_name].select())
+        self._connector.execute(self._tables[table_name].select())
 
     @staticmethod
     def generate_columns(column_infos: dict, primarykey_index):
